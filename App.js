6914b5d1de2f0af3554604debc7d81d665fb3d4e
Michael ilkanayev <93651794+michaelilkanayev1997@users.noreply.github.com>
2023-09-23 23:48:41 +0300

    Added RestaurantInfoCard & RestaurantScreen

--- App.js
+++ App.js
@@ -1,24 +1,14 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { StatusBar, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 
 export default function App() {
   return (
     <>
-      <SafeAreaView style={styles.container}>
-        <View style={styles.search}>
-          <Text>Search</Text>
-        </View>
-        <View style={styles.list}>
-          <Text>List</Text>
-        </View>
-      </SafeAreaView>
+      <RestaurantsScreen />
       <ExpoStatusBar style="auto" />
     </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: { flex: 1, marginTop: StatusBar.currentHeight },
-  search: { padding: 16, backgroundColor: "green" },
-  list: { flex: 1, padding: 16, backgroundColor: "blue" },
-});
+const styles = StyleSheet.create({});
