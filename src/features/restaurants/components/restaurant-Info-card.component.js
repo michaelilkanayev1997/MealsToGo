edf96877e03acd1c20bc5966117878f4272246a8
Michael ilkanayev <93651794+michaelilkanayev1997@users.noreply.github.com>
2023-09-25 20:35:47 +0300

    Loading Custom Fonts

--- src/features/restaurants/components/restaurant-Info-card.component.js
+++ src/features/restaurants/components/restaurant-Info-card.component.js
@@ -3,16 +3,17 @@ import styled from "styled-components/native";
 import { Card } from "react-native-paper";
 
 const RestaurantCard = styled(Card)`
-  background-color: white;
+  background-color: ${(props) => props.theme.colors.bg.primary};
 `;
 
 const RestaurantCardCover = styled(Card.Cover)`
-  padding: 20px;
-  background-color: white;
+  padding: ${(props) => props.theme.space[3]};
+  background-color: ${(props) => props.theme.colors.bg.primary};
 `;
 
 const Title = styled.Text`
-  padding: 16px;
+  font-family: ${(props) => props.theme.fonts.body};
+  padding: ${(props) => props.theme.space[3]};
   color: ${(props) => props.theme.colors.ui.primary};
 `;
 
