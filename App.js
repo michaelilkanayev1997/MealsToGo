bf6c8171fecfd6564a97b62f5735e8207c8c5884
Michael ilkanayev <93651794+michaelilkanayev1997@users.noreply.github.com>
2023-09-27 23:36:53 +0300

    Added mock data

--- App.js
+++ App.js
@@ -12,6 +12,7 @@ import { theme } from "./src/infrastructure/theme";
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 import { SafeArea } from "./src/components/utility/safe-area.component";
 import { Ionicons } from "@expo/vector-icons";
+import { restaurantsRequest } from "./src/services/restaurants/restaurants.service";
 
 // Force LTR text direction
 I18nManager.allowRTL(false);
