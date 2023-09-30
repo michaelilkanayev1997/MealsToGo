cd54523d4e4b53b5d428884fa85fb47fc160d0f5
Michael ilkanayev <93651794+michaelilkanayev1997@users.noreply.github.com>
2023-09-30 22:51:47 +0300

    Added Custom Map Callout

---
+++ src/components/restaurant/compact-restaurant-info.component.js
@@ -0,0 +1,47 @@
+import styled from "styled-components/native";
+import WebView from "react-native-webview";
+import { Platform } from "react-native";
+import { Text } from "../typography/text.component";
+
+const CompactViewAndroid = styled.View`
+  border-radius: 10px;
+  width: 120px;
+  height: 100px;
+  overflow: hidden;
+`;
+
+const CompactImage = styled.Image`
+  border-radius: 10px;
+  width: 120px;
+  height: 100px;
+`;
+
+const CompactWebview = styled(WebView)`
+  border-radius: 10px;
+  width: 120px;
+  height: 100px;
+  overflow: hidden;
+`;
+
+const Item = styled.View`
+  padding: 10px;
+  max-width: 120px;
+  align-items: center;
+`;
+
+const isAndroid = Platform.OS === "android";
+
+export const CompactRestaurantInfo = ({ restaurant }) => {
+  const Image = isAndroid ? CompactWebview : CompactImage;
+
+  return (
+    <Item>
+      <CompactViewAndroid>
+        <Image source={{ uri: restaurant.photos[0] }} />
+      </CompactViewAndroid>
+      <Text center variant="caption" numberOfLines={3}>
+        {restaurant.name}
+      </Text>
+    </Item>
+  );
+};
--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -1,9 +1,10 @@
 import { useContext, useState, useEffect } from "react";
-import MapView, { Marker } from "react-native-maps";
+import MapView, { Marker, Callout } from "react-native-maps";
 import styled from "styled-components/native";
 import { Search } from "../components/search.component";
 import { LocationContext } from "../../../services/location/location.context";
 import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
+import { MapCallout } from "../components/map-callout.component";
 
 const Map = styled(MapView)`
   height: 100%;
@@ -45,7 +46,11 @@ export const MapScreen = () => {
                 latitude: restaurant.geometry.location.lat,
                 longitude: restaurant.geometry.location.lng,
               }}
-            />
+            >
+              <Callout>
+                <MapCallout restaurant={restaurant} />
+              </Callout>
+            </Marker>
           );
         })}
       </Map>
