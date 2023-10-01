07af79e8c7ec91763e08538a1323dfe69ff38c91
Michael ilkanayev <93651794+michaelilkanayev1997@users.noreply.github.com>
2023-10-01 23:35:58 +0300

    Added Account Screen Buttons

--- src/infrastructure/navigation/account.navigator.js
+++ src/infrastructure/navigation/account.navigator.js
@@ -13,6 +13,6 @@ export const AccountNavigator = () => (
   >
     <Stack.Screen name="Main" component={AccountScreen} />
     <Stack.Screen name="Login" component={LoginScreen} />
-    <Stack.Screen name="Regiser" component={RegisterScreen} />
+    <Stack.Screen name="Register" component={RegisterScreen} />
   </Stack.Navigator>
 );
